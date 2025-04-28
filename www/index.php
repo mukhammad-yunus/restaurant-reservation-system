111684c2f13952880aa582ee77f3ed8bd4541125
Mukhammadyunus <dr.mukhammadyunus@gmail.com>
2025-04-28 15:39:01 +0200

    Home page

---
+++ www/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Restaurant Reservation system</title>
+</head>
+
+<body>
+
+  <body>
+    <h1>
+      Restaurant Reservation System
+    </h1>
+    <h2>Customer Actions</h2>
+    <ul>
+      <li><a href="make_reservation.php">Make a Reservation</a></li>
+      <li><a href="view_reservation.php">View Reservation</a></li>
+      <li><a href="cancel_reservation.php">Cancel Reservation</a></li>
+    </ul>
+    <h2>Staff Actions</h2>
+    <ul>
+      <li><a href="staff_manage.php">Manage Reservations</a></li>
+    </ul>
+  </body>
+</body>
+
+</html>
